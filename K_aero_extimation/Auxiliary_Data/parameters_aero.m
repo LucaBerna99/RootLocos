9263d4d7a51e75d18c54eb6e8cf7945d50a7dbbd
LucaBerna99 <luca.bendazzoli@mail.polimi.it>
2023-03-07 23:54:20 +0100

    Both mutual coefficients have been extimated. to be checked

---
+++ K_aero_extimation/Auxiliary_Data/parameters_aero.m
@@ -0,0 +1,39 @@
+%% PARAMETERS
+ %DC Motor  
+    V_nom = 18.0;       %V
+    tau_nom = 22.0;     %mN-m
+    omega_nom = 3050;   %RPM
+    Inom = 0.540;       %A
+    Rm = 8.4;           %?
+    kt = 0.042;         %N-m/A
+    km = 0.042;         %V/(rad/s)
+    Jm = 4.0e-6;        %g-m2
+    Lm = 1.16;          %mH
+%Aero Body
+    Mb = 1.075;         %kg
+    Dm = -7.59/1000;    %mm/1000 -> m
+    Jp = 2.15e-2;       %kg-m2
+    Jy = 2.37e-2;       %kg-m2
+    Dt = 15.8/100;      %cm/100 -> m
+%Motor and Pitch Encoders
+    Pitch_encoder_line_count = 512;             %lines/rev
+    Pitch_encoder_line_count_quad = 2048;       %lines/rev
+    Pitch_encoder_res = 0.176;                  %deg/count
+%Yaw Encoder
+    Yaw_encoder_line_count =  1024;             %lines/rev
+    Yaw_encoder_line_count_quadr = 4096;        %lines/rev
+    Yaw_encoder_res = 0.088;                    %deg/count
+%Amplifier
+    
+    %Amplifier type PWM
+    peak_current = 2;            %A
+    countinuous_current = 0.5;   %A
+    V_range = [-18, +18];        %V
+    V_range_max =  [- 24, +24];  %V
+
+%% Other Constants
+
+g = 9.81;
+
+B_friction = 7e-5;
+m_current = 3e-4; % computed in CURRENT_MAP_extimation
---
+++ MutualEffect/mutualEffect.m
@@ -0,0 +1,128 @@
+%% Load Data
+
+mutual0 = load('M0steps_MutualYaw.mat');
+
+start_m0 = 120/0.002;
+finish_m0 = 270/0.002;
+t_m0 = mutual0.data(1,start_m0:finish_m0);
+yaw_m0 = mutual0.data(3,start_m0:finish_m0)*Yaw_encoder_res;
+V0_m0 = mutual0.data(4,start_m0:finish_m0);
+
+k_aero = 0.088;
+
+
+
+%% Effect of M0 on the yaw movement
+
+    %% FITTING
+    
+    yaw_fit_m0 = @(yaw_par,t) yaw_par(1).*t.^4 + yaw_par(2).*t.^3 + yaw_par(3).*t.^2 + yaw_par(4).*t + yaw_par(5);
+    yaw_par0_m0 = [1,1,1,1,1,1];
+    yaw_par_m0 = lsqcurvefit(yaw_fit_m0, yaw_par0_m0, t_m0, yaw_m0);
+    
+    A_m0 = yaw_par_m0(1);
+    B_m0 = yaw_par_m0(2);
+    C_m0 = yaw_par_m0(3);
+    D_m0 = yaw_par_m0(4);
+    E_m0 = yaw_par_m0(5);
+    
+    phi_yaw_m0 = A_m0.*t_m0.^4 + B_m0.*t_m0.^3 + C_m0.*t_m0.^2 + D_m0.*t_m0 + E_m0;
+    phi_d_yaw_m0 = 4*A_m0.*t_m0.^3 + 3*B_m0.*t_m0.^2 + 2*C_m0.*t_m0 + D_m0;
+    phi_dd_yaw_m0 = 12*A_m0.*t_m0.^2 + 6*B_m0.*t_m0 + 2*C_m0;
+    
+    %% PLOT DERIVATIVES
+    
+    set(figure, "WindowStyle", "docked");
+    grid;
+    hold on;
+    plot(t_m0, yaw_m0,'b','LineWidth',2);
+    plot(t_m0, phi_yaw_m0, 'r');
+    plot(t_m0, phi_d_yaw_m0, 'g');
+    plot(t_m0, phi_dd_yaw_m0, 'b');
+    legend("measured \phi","\phi","\phi_d", "\phi_{dd}");
+    hold off;
+    
+    
+    
+    %% EQUATION
+
+    for i = 1:1:length(t_m0)
+
+        M0(i) = (k_yaw * phi_d_yaw_m0(i) + Jy * phi_dd_yaw_m0(i))/Dt;
+        F0(i) = k_aero*V0_m0(i)^2;
+        k_mutual0(i) = M0(i) / F0(i);
+
+    end
+
+    set(figure, "WindowStyle", "docked");
+    grid;
+    hold on;
+    plot(t_m0, k_mutual0,'LineWidth',1.5);
+    hold off;
+
+    m_M0_yaw = abs(mean(k_mutual0));
+
+
+
+%% Load data
+
+mutual1 = load('M1steps_Mutual.mat');
+
+start_m1 = 1; %120/0.002;
+finish_m1 = 90/0.002;
+t_m1 = mutual1.data(1,start_m1:finish_m1);
+pitch_m1 = mutual1.data(3,start_m1:finish_m1)*Pitch_encoder_res;
+V1_m1 = mutual1.data(2,start_m1:finish_m1);
+
+set(figure, "WindowStyle", "docked");
+grid;
+hold on;
+plot(t_m1, pitch_m1,'LineWidth',0.5);
+plot(t_m1, V1_m1,'LineWidth',1);
+hold off;
+
+
+%% Effect of M0 on the yaw movement
+    
+    
+    fit_pitch = [0.528, 3.696, 7.744];
+    fit_voltage = [2.0, 4.0, 6.0];
+    
+    for i=1:1:length(t_m1)
+        
+        if i < (30/0.002)
+            scale_pitch(i) = fit_pitch(1);
+        elseif i >= (30/0.002) && i < (60/0.002)
+            scale_pitch(i) = fit_pitch(2);
+        elseif i >= (60/0.002)
+            scale_pitch(i) = fit_pitch(3);
+        end
+
+    end
+
+    
+    set(figure, "WindowStyle", "docked");
+    grid;
+    hold on;
+    plot(t_m1, scale_pitch,'LineWidth',1);
+    plot(t_m1, pitch_m1,'LineWidth',0.5);
+    hold off;
+
+
+    for i =1:length(fit_voltage)
+        M1(i) = Mb*g*Dm * sin(fit_pitch(i)*pi/180) / Dt;
+        k_mutual1(i) = M1(i) / (k_aero * fit_voltage(i)^2);
+    end
+
+    m_M1_pitch =abs(mean(k_mutual1));
+
+    set(figure, "WindowStyle", "docked");
+    grid;
+    hold on;
+    plot(k_mutual1,'LineWidth',1);
+    hold off;
+
+
+
+
+
